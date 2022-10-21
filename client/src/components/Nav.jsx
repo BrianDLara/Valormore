6410e0c35f3253f9079dc100837a28d928066693
Brian Lara <Briandlara1@gmail.com>
2022-10-21 11:18:35 -0400

    added a nav-bar

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -4,8 +4,10 @@ import React from 'react'
 
 const Nav = () => {
   return(  
-    <div>
-        <logo></logo>
+    <div className = "nav-container">
+        <logo >
+            <img src="images/logo.png" alt="website logo" className='logo'/>
+        </logo>
         <div>
             <NavLink to = '/' className="nav-link"> Home </NavLink>
             <NavLink to = '/products' className="nav-link"> Products </NavLink>
