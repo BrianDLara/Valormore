9d2810c2f522cf220fa83a190caf834a1d4d9245
Brian Lara <Briandlara1@gmail.com>
2022-10-22 21:14:40 -0400

    created an axios call that gets a single product

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -4,13 +4,27 @@ import axios from 'axios'
 import { useParams } from 'react-router-dom'
 
 const Product = () => {
-   
+    let { productId } = useParams()
+
+
+    const [product, setProduct] = useState(null)
+    
+    const findProductById = async () => {
+        const response = await axios.get(`http://localhost:3001/api/product/${productId}`)
+        setProduct(response.data.product)
+        console.log(response.data.product)
+        
+    }
+
+    useEffect(() => {
+        findProductById()
+      }, [])
+
     return (
-    <div className= 'product'>
+    <div >
         
     </div>
-    )
-   
+    ) 
 }
 
 export default Product
