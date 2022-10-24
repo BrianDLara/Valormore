213545fdac4413ca7f74534abcd477fc367d0b0d
Brian Lara <Briandlara1@gmail.com>
2022-10-24 11:18:05 -0400

    created a add comment page with a simple structure

---
+++ client/src/pages/AddReview.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+const AddReview = () => {
+  const [review, getReview] = useState([])
+  const initialState = {
+    subject: '',
+    description: ''
+  }
+
+  return <div></div>
+}
+
+export default AddReview
