bd1028a4b13b7d2081b5c5d78713d1ca08de3233
Brian Lara <Briandlara1@gmail.com>
2022-10-24 22:22:38 -0400

    added an edit button for every review

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 const Reviews = (props) => {
   return (
     <div className='reviews-container'>
+      <ul id="edit-button" className='list-drop'>
+        <li className='sub-list'>Edit</li>
+        <li className='sub-list'>Delete</li>
+      </ul>
         <h2 className='review-title'>{props.title}</h2>
         <h3 className='review-description'>{props.description}</h3>
     </div>
